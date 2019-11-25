0b0f7c7edf4b7bdb5ea6c58f2de95202ee88dd96
aaldyy <aldirenaldialamin@gmail.com>
2019-11-25 23:26:53 +0800

    test_aldy

---
+++ koneksidatabases/Form1.vb
@@ -0,0 +1,103 @@
+﻿Imports System.Data.Odbc
+Public Class Form1
+    Sub TampilGrid()
+        bukakoneksi()
+
+
+        DA = New OdbcDataAdapter("Select * from  tbl_mahasiswa", CONN)
+        DS = New DataSet
+        DA.Fill(DS, "tbl_mahasiswa")
+        DataGridView1.DataSource = DS.Tables("tbl_mahasiswa")
+        tutupkoneksi()
+
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        TampilGrid()
+    End Sub
+
+    Sub MunculCombo()
+        ComboBox1.Items.Add("Ilmu Komputer")
+        ComboBox1.Items.Add("Kimia")
+        ComboBox1.Items.Add("Fisika")
+        ComboBox1.Items.Add("Matematika")
+    End Sub
+
+    Sub KosongkanData()
+        TextBox1.Text = ""
+        TextBox2.Text = ""
+        TextBox3.Text = ""
+        TextBox4.Text = ""
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If TextBox1.Text = "" Or TextBox2.Text = "" Or TextBox3.Text = "" Or TextBox4.Text = "" Then
+            MsgBox("Silahkan Isi Semua From")
+
+        Else
+            bukakoneksi()
+            Dim simpan As String = "insert into tbl_mahasiswa values('" & TextBox1.Text & "','" & TextBox2.Text & "','" & TextBox3.Text & "','" & TextBox4.Text & "','" & ComboBox1.Text & "')"
+
+            CMD = New OdbcCommand(simpan, CONN)
+            CMD.ExecuteNonQuery()
+            MsgBox("Input data Berhasil")
+            TampilGrid()
+            KosongkanData()
+
+            tutupkoneksi()
+        End If
+
+    End Sub
+    Private Sub TextBox1_KeyPress1(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles TextBox1.KeyPress
+        TextBox1.MaxLength = 6
+        If e.KeyChar = Chr(13) Then
+            bukakoneksi()
+            CMD = New OdbcCommand("Select * from tbl_mahasiswa where NIMMHS='" & TextBox1.Text & "'", CONN)
+            RD = CMD.ExecuteReader
+            RD.Read()
+            If Not RD.HasRows Then
+                MsgBox("NIM tidak Ada,Silahkan coba lagi!")
+                TextBox1.Focus()
+            Else
+                TextBox2.Text = RD.Item("namamhs")
+                TextBox3.Text = RD.Item("alamatmhs")
+                TextBox4.Text = RD.Item("teleponmhs")
+                ComboBox1.Text = RD.Item("jurusanmhs")
+                TextBox2.Focus()
+            End If
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        bukakoneksi()
+        Dim edit As String = "update tbl_mahasiswa set
+            namamhs='" & TextBox2.Text & "',
+            alamatmhs='" & TextBox3.Text & "',
+            teleponmhs='" & TextBox4.Text & "',
+            jurusanmhs='" & ComboBox1.Text & "' where nimmhs='" & TextBox1.Text & "'"
+
+        CMD = New OdbcCommand(edit, CONN)
+        CMD.ExecuteNonQuery()
+        MsgBox("Data Berhasil diUpdate")
+        TampilGrid()
+        KosongkanData()
+        tutupkoneksi()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+        If TextBox1.Text = "" Then
+            MsgBox("Silahkan Pilih Data yang akan dihapus dengan masukkan NIm DAN ENTER")
+        Else
+            If MessageBox.Show("Yakin akan dihapus...?", "", MessageBoxButtons.YesNo) = Windows.Forms.DialogResult.Yes Then
+                bukakoneksi()
+                Dim hapus As String = "delete From tbl_mahasiswa where nimmhs='" & TextBox1.Text & "'"
+                CMD = New OdbcCommand(hapus, CONN)
+                CMD.ExecuteNonQuery()
+                TampilGrid()
+                KosongkanData()
+                tutupkoneksi()
+            End If
+        End If
+    End Sub
+End Class
